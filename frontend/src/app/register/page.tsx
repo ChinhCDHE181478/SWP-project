57be4d0d2a484692c2b81e633ee50742fe57ed25
ChinhCDHE181478 <quansuper000@gmail.com>
2025-02-07 01:54:22 +0700

    complete login page frontend

--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -11,28 +11,28 @@ import {
 import Link from "next/link";
 
 export const metadata: Metadata = {
-  title: "Login",
+  title: "Register",
 };
 
-const LoginPage = () => {
+const RegisterPage = () => {
   return (
-    <div className="w-screen bg-[url('/login/bg.jpg')] bg-cover bg-left bg-no-repeat md:justify-items-end justify-items-center">
-      <div className="flex flex-col h-screen w-min-[350px] md:w-[700px] md:px-28 items-center justify-center md:mx-20">
-        <Card className="px-10 py-10 max-w-3xl w-full rounded-lg dark bg-white lg:p-5 bg-opacity-55 backdrop-blur-lg">
+    <div className="w-screen bg-[url('/login/bg2.jpg')] bg-cover bg bg-center md:bg-left bg-no-repeat md:justify-items-end justify-items-center">
+      <div className="flex flex-col h-screen w-min-[350px] md:w-[700px] md:px-28 items-center justify-center">
+        <Card className="px-10 py-10 max-w-3xl w-full rounded-lg dark bg-white p-0 lg:p-5">
           <CardHeader className="">
             <div className="flex justify-center items-center">
               <LogoIcon className="w-[150px] h-auto" />
             </div>
-            <CardTitle className="font-bold text-3xl">Đăng nhập</CardTitle>
+            <CardTitle className="font-bold text-3xl">Login</CardTitle>
           </CardHeader>
           <CardContent>
             <LoginForm />
           </CardContent>
           <CardFooter className="justify-center">
             <p>
-              Bạn chưa có tài khoản?{" "}
+              Need an Account?{" "}
               <span>
-                <Link className="text-blue-500" href={"/register"}>Đăng ký ngay</Link>
+                <Link href={"/register"}>Sign Up Now</Link>
               </span>
             </p>
           </CardFooter>
@@ -41,4 +41,4 @@ const LoginPage = () => {
     </div>
   );
 };
-export default LoginPage;
+export default RegisterPage;
---
+++ frontend/src/helper/urlPath.ts
@@ -0,0 +1,6 @@
+export const LOGIN = "/identity/auth/login";
+export const REFRESH = "/identity/auth/refresh";
+export const REGISTER = "/identity/auth/register";
+export const LOGOUT = "/identity/auth/logout";
+
+
